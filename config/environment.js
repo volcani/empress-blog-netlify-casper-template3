04eda4f483ac4a8441b766820f0286c874d1d156
volcani <catofulthar@gmail.com>
2020-08-27 14:15:20 +0900

    test

--- config/environment.js
+++ config/environment.js
@@ -24,7 +24,7 @@ module.exports = function(environment) {
     },
 
     blog: {
-      title: 'empress-blog',
+      title: '夜猫',
       description: 'Static Blog System - Built with Ember',
       coverImage: '/images/blog-cover.jpg',
 
